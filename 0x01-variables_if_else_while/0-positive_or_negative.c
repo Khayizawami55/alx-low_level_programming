ec2a8588b7e65c0d2851b22fddac3974cb734277
Ntokozo Mkhize <brian.mkhize18@gmail.com>
2023-06-25 09:56:13 -0700

    ariables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,6 @@
-#include <stdlib.h>                             #include <time.h>                               #include <stdio.h>                              
+#include <stdlib.h>                             
+#include <time.h>                               
+#include <stdio.h>                              
 /**
  * main - Prints a random number and states whether
  *        it is positive, negative, or zero.
