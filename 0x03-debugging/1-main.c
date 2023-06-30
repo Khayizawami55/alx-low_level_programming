1474c1300855e687ddcd8a9d726a96dd2d764034
Ntokozo Mkhize <brian.mkhize18@gmail.com>
2023-06-30 01:13:40 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,14 +10,14 @@ int main(void)
 
         printf("Infinite loop incoming :(\n");
 
-        i = 0;
+        i = 24; // Set the initial value of i to 24
 
-        while (i < 10)
+        while (i < 10) // The condition will be false, and the loop won't be executed
         {
                 i++; // Increment i
         }
 
-        printf("%d\n", i); // Print the value of i after the loop
+        printf("%d\n", i); // Print the value of i
 
         printf("Infinite loop avoided! \\o/\n");
 
