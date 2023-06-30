5c46bd0fb50f1a2cabbabbe480272e8052154841
Ntokozo Mkhize <brian.mkhize18@gmail.com>
2023-06-30 01:17:01 -0700

    Debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - prints the value of 24
+ * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
-        int i;
+ int i;
 
-        printf("Infinite loop incoming :(\n");
+ printf("Infinite loop incoming :(\n");
 
-        i = 24; // Set the initial value of i to 24
+ i = 0;
 
-        while (i < 10) // The condition will be false, and the loop won't be executed
-        {
-                i++; // Increment i
-        }
+ /*while (i < 10)*/
+ /*{*/
+ /* putchar(i);*/
+ /*}*/
 
-        printf("%d\n", i); // Print the value of i
+ printf("Infinite loop avoided! \\o/\n");
 
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+ return (0);
 }
